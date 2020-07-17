fc067af97a15ee693264b1f9c19c1a5f8af779ac
Kelvin Lehrback <kelvin.lehrback@gmail.com>
2020-07-17 15:48:15 -0300

    Delimiter move player

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,9 +8,6 @@ public class Player : MonoBehaviour
 {
     //variables
     public float speed = 5.0f;
-    public float horizontalInput;
-    public float verticalInput;
-
     // Start is called before the first frame update
     void Start()
     {
@@ -21,16 +18,42 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log("X pos: " + transform.position.x);
+        //Debugs
+        Debug.Log("X pos: " + transform.position.x + ", Y pos: " + transform.position.y);
+        //Debug.Log("Y pos: " + transform.position.y);
 
         //Move player to left and right
-        horizontalInput = Input.GetAxis("Horizontal");
+        float horizontalInput = Input.GetAxis("Horizontal");
         //new Vector3(1,0,0) * 1(TD) * inputUser(1 or -1) * speed
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
 
         //Move player to up and down
-        verticalInput = Input.GetAxis("Vertical");
+        float verticalInput = Input.GetAxis("Vertical");
         //new Vector3(0,1,0) * 1(TD) * inputUser(1 or -1) * speed
         transform.Translate(Vector3.up * Time.deltaTime * verticalInput * speed);
+
+        //Delimite space to moving player
+        float max_x_range = 8.08f;
+        float min_x_range = -8.02f;
+        float max_y_range = 3.91f;
+        float min_y_range = -3.97f;
+
+        if(transform.position.x < min_x_range)
+        {
+            transform.position = new Vector3(min_x_range, transform.position.y, 0);
+        }
+        else if(transform.position.x > max_x_range)
+        {
+            transform.position = new Vector3(max_x_range, transform.position.y, 0);
+        }
+        
+        if(transform.position.y < min_y_range)
+        {
+            transform.position = new Vector3(transform.position.x, min_y_range, 0);
+        }
+        else if(transform.position.y > max_y_range)
+        {
+            transform.position = new Vector3(transform.position.x, max_y_range, 0);
+        }
     }
 }
