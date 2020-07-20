88af8e836be99b90978e425dca6f9ac1b9e4adbe
Kelvin Lehrback <kelvin.lehrback@gmail.com>
2020-07-20 16:12:31 -0300

    Destruindo o laser apois sair de cena

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    public float speed = 10.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Posicao do objeto associado ao script
+        //Movendo-o para cima numa velocidade X * o tempo
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
+
+        //Quando o laser sai de cena, destruimos o objeto
+        if(transform.position.y >= 6)
+        {
+            //Destroy(this.gameObject)
+            Destroy(gameObject);
+        }
+    }
+}
