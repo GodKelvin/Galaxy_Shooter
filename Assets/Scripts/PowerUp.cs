6543f0d66792d26e481d4b18101e08c32d1c1b78
Kelvin Lehrback <kelvin.lehrback@gmail.com>
2020-07-21 16:54:36 -0300

    Inserindo colisões

---
+++ Assets/Scripts/PowerUp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    //movimentando para fora da cena 3m/s
+    [SerializeField]
+    private float _speed = 3.0f;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        //Fazendo-o cair em tempo real 3m/s
+        transform.Translate(Vector3.down * Time.deltaTime * _speed);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("OPA: " + other.name);
+    }
+}
