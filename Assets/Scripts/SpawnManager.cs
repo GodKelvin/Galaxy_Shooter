530fbea557adb842d8780b716fbab3d6ecf177a4
Kelvin Lehrback <kelvin.lehrback@gmail.com>
2020-08-05 16:20:01 -0300

    EnemySpawn configurado

---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject _enemyPrefab = null;
+
+    //Criando um array de GameObjects
+    [SerializeField]
+    private GameObject[] _powerUps;
+
+    [SerializeField]
+    private float _timeSpawnEnemy = 3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(EnemySpawn());
+    }
+
+    IEnumerator EnemySpawn()
+    {
+        while(true)
+        {
+            //Enemy contem uma funcao de spawn aleatoria
+            Instantiate(_enemyPrefab, transform.position, Quaternion.identity);
+            yield return new WaitForSeconds(_timeSpawnEnemy);
+        }
+    }
+
+
+}
